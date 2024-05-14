edcb6bad65eb37556a2da5a16787c65363328271
rbaxim <142934208+rbaxim@users.noreply.github.com>
2024-05-13 20:32:14 -0400

    Update CookieClicker.js

--- CookieClicker.js
+++ CookieClicker.js
@@ -1,17 +1,33 @@
-function main(Delay){
+function main(Delay) {
   setInterval(function() {
+    //Golden Cookies
     Game.shimmers.forEach(function(shimmer) {
-        if (shimmer.type == "golden") { 
-          shimmer.pop() 
-        }
+      if (shimmer.type == "golden") { 
+        shimmer.pop(); 
+      }
+    });
+
+    // Click The Cookie
     Game.ClickCookie();
-    for (i = 0; i < Game.wrinklers.length; i++) { Game.wrinklers[i].type = 1; }
-    Game.priceIncrease = 1
-    Game.ObjectsById[Math.random(0, 19)].level += 1000;
-    Game.recalculateGains=1;
+
+    // Wrinklers
+    for (i = 0; i < Game.wrinklers.length; i++) { 
+      Game.wrinklers[i].type = 1; 
+    }
+
+    // Cost becomes normal
+    Game.priceIncrease = 1;
+
+    // Upgrade Buildings
+    var randomBuildingId = Math.floor(Math.random() * 20);
+    Game.ObjectsById[randomBuildingId].level += 1000;
+    Game.recalculateGains = 1;
+      
   }, Delay);
+  
   setInterval(function() {
-    Game.PopRandomWrinkler()
-  }, 10000)
+    // Pop Wrinkler
+    Game.PopRandomWrinkler();
+  }, 10000);
   customTickers.push("Made using Cookie Clicker Hacks");
 }
