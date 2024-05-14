de427fe72f4d577d363aaf84b9c251bb4befb6aa
rbaxim <142934208+rbaxim@users.noreply.github.com>
2024-05-14 16:54:46 -0400

    Update CookieClicker.js

--- CookieClicker.js
+++ CookieClicker.js
@@ -1,41 +1,46 @@
-var Delay = 1;
-var LaggyDelay = 6000;
-Objects = ["Cursor", "Grandma", "Farm", "Mine", "Factory", "Bank", "Temple", "Wizard tower", "Shipment", "Alchemy lab", "Portal", "Time machine", "Antimatter condesnser", "Prism", "Chancemaker", "Fractal engine", "Javascript console", "Idleverse", "Cortex baker", "You"]
-setInterval(function() {
-  //Golden Cookies
-  Game.shimmers.forEach(function(shimmer) {
-    if (shimmer.type == "golden") { 
-      shimmer.pop(); 
-    }
-  });
-
-  // Click The Cookie
-  Game.ClickCookie();
-
-  // Wrinklers
-  for (i = 0; i < Game.wrinklers.length; i++) { 
-    Game.wrinklers[i].type = 1; 
+(function () {
+  var Delay = 1;
+  var LaggyDelay = 6000;
+  Objects = ["Cursor", "Grandma", "Farm", "Mine", "Factory", "Bank", "Temple", "Wizard tower", "Shipment", "Alchemy lab", "Portal", "Time machine", "Antimatter condesnser", "Prism", "Chancemaker", "Fractal engine", "Javascript console", "Idleverse", "Cortex baker", "You"]
+  var customTickers = ["Made using Cookie Clicker Hacks"]
+  customTickersFunction = function() { 
+    return customTickers;
   }
-
-  // Buildings
-  Game.priceIncrease = 1;
-    
-}, Delay);
-// Laggy Features
-if (LaggyDelay > 5000){
   setInterval(function() {
-    var randomBuildingId = Math.floor(Math.random() * 20);
-    Game.ObjectsById[randomBuildingId].level += 1000;
-    Game.recalculateGains = 1;
-    for (var i = 0; i < Objects.length; i++){
-      Game.Objects[Objects[i]].amount += 5000;
+    //Golden Cookies
+    Game.shimmers.forEach(function(shimmer) {
+      if (shimmer.type == "golden") { 
+        shimmer.pop(); 
+      }
+    });
+  
+    // Click The Cookie
+    Game.ClickCookie();
+  
+    // Wrinklers
+    for (i = 0; i < Game.wrinklers.length; i++) { 
+      Game.wrinklers[i].type = 1; 
     }
-  }, LaggyDelay);
-} else {
-  console.error("Too slow for laggy features")
-}
-setInterval(function() {
-  // Pop Wrinkler
-  Game.PopRandomWrinkler();
-}, 10000);
-customTickers.push("Made using Cookie Clicker Hacks");
+  
+    // Buildings
+    Game.priceIncrease = 1;
+      
+  }, Delay);
+  // Laggy Features
+  if (LaggyDelay > 5000){
+    setInterval(function() {
+      var randomBuildingId = Math.floor(Math.random() * 20);
+      Game.ObjectsById[randomBuildingId].level += 1000;
+      Game.recalculateGains = 1;
+      for (var i = 0; i < Objects.length; i++){
+        Game.Objects[Objects[i]].amount += 5000;
+      }
+    }, LaggyDelay);
+  } else {
+    console.error("Too slow for laggy features")
+  }
+  setInterval(function() {
+    // Pop Wrinkler
+    Game.PopRandomWrinkler();
+  }, 10000);
+})()
